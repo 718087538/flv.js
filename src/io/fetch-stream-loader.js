78d3f1972683dba7cae1501408feb6463ea118ee
kason <718087538@qq.com>
2020-08-25 17:49:42 +0800

    添加AbortController来终止fetch，修复播放器在destroy后仍然会下载数据的问题

--- src/io/fetch-stream-loader.js
+++ src/io/fetch-stream-loader.js
@@ -117,6 +117,13 @@ class FetchStreamLoader extends BaseLoader {
             params.referrerPolicy = dataSource.referrerPolicy;
         }
 
+         // add abort controller, by wmlgl 2019-5-10 12:21:27
+         if (self.AbortController) {
+            this._abortController = new self.AbortController();
+            params.signal = this._abortController.signal;     
+        }
+
+
         this._status = LoaderStatus.kConnecting;
         self.fetch(seekConfig.url, params).then((res) => {
             if (this._requestAbort) {
@@ -152,6 +159,9 @@ class FetchStreamLoader extends BaseLoader {
                 }
             }
         }).catch((e) => {
+            if (this._abortController && this._abortController.signal.aborted) {
+                return;
+            }
             this._status = LoaderStatus.kError;
             if (this._onError) {
                 this._onError(LoaderErrors.EXCEPTION, {code: -1, msg: e.message});
@@ -163,6 +173,9 @@ class FetchStreamLoader extends BaseLoader {
 
     abort() {
         this._requestAbort = true;
+        if (this._abortController) {
+            this._abortController.abort();
+        }
     }
 
     _pump(reader) {  // ReadableStreamReader
